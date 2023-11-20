a6306e8f1232420273e569de0254276527cbd88e
Behdad Esfahbod <behdad@behdad.org>
2023-11-20 13:27:25 -0700

    [gen-reports] Time everything

--- gen-reports.sh
+++ gen-reports.sh
@@ -13,9 +13,9 @@ if [ -z "$out" ]; then
 fi
 
 git submodule init
-(cd "$dirname/submodules/google-fonts" && git pull)
-(cd "$dirname/submodules/fonttools" && git pull)
-(cd "$dirname/submodules/fonttools" && python setup.py build_ext -i)
+(cd "$dirname/submodules/google-fonts" && time git pull)
+(cd "$dirname/submodules/fonttools" && time git pull)
+(cd "$dirname/submodules/fonttools" && time python setup.py build_ext -i)
 
 time find "$dir" -name '*\[*.ttf' -print |
   xargs ls --sort=size |
@@ -24,17 +24,17 @@ time find "$dir" -name '*\[*.ttf' -print |
 
 if $clear; then
   echo "Clearing $out"
-  rm -f $out/*.ttf.{pdf,html,txt}
+  time rm -f $out/*.ttf.{pdf,html,txt}
 fi
 
 echo "Moving reports to $out"
-find $dir \( -name '*.ttf.pdf' -o -name '*.ttf.html' -o -name '*.ttf.txt' \) -print0 |
+time find $dir \( -name '*.ttf.pdf' -o -name '*.ttf.html' -o -name '*.ttf.txt' \) -print0 |
   xargs -0 -I{} mv {} "$out"
 
 echo "Removing empty reports"
-find "$out" -name '*.ttf.txt' -size 0 |
+time find "$out" -name '*.ttf.txt' -size 0 |
   while read x; do rm -f "$x" "${x%txt}pdf" "${x%txt}html"; done
 
 echo "Gzipping the HTML reports"
-find "$out" -name '*.ttf.html' |
+time find "$out" -name '*.ttf.html' |
   while read x; do gzip "$x"; done
