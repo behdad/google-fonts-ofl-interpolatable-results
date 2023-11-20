a3083a4507b069441ddf17f7c4e9b5a7263dc90d
Behdad Esfahbod <behdad@behdad.org>
2023-11-20 15:13:13 -0700

    Improve diff tools

--- diff-ttf-html-bz2.sh
+++ diff-ttf-html-bz2.sh
@@ -9,7 +9,7 @@ bunzip2 -c "$1" > "$dir/1.html"
 bunzip2 -c "$2" > "$dir/2.html"
 
 echo "Diffing $dir/1.html and $dir/2.html to $dir/diff.html"
-"$dirname/html-diff.sh" "$dir/1.html" "$dir/2.html" > "$dir/diff.html"
+"$dirname/html-diff.sh" --title "$3" "$dir/1.html" "$dir/2.html" > "$dir/diff.html"
 
 #rm -f "$dir/1.html" "$dir/2.html"
 
--- html-diff.sh
+++ html-diff.sh
@@ -2,9 +2,20 @@
 
 echo "<!DOCTYPE html>"
 echo "<html><body align=center>"
+
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 <old-file>.html <new-file>.html"
+	exit 1
+fi
+
+if [ "$1" = "--title" ]; then
+	echo "<h1>$2</h1>"
+	shift 2
+fi
+
 diff \
 	--unchanged-line-format='' \
-	--old-line-format='<span style="font-size: 100pt; color: red">-</span>%L' \
-	--new-line-format='<span style="font-size: 100pt; color: green">+</span>%L' \
+	--old-line-format='<div style="background: #F88">%L</div>' \
+	--new-line-format='<div style="background: #8F8">%L</div>' \
 	"$@"
 echo "</body></html>"
