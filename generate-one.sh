295b8005fc994bb1f768fd2928b2357d730eeafc
Behdad Esfahbod <behdad@behdad.org>
2023-11-20 16:38:54 -0700

    Add generate-one.sh

---
+++ generate-one.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+dirname=`dirname $0`
+ttf=$1
+basename=`basename $ttf`
+out="$dirname/reports/$basename"
+
+"$dirname"/submodules/fonttools/fonttools varLib.interpolatable "$ttf" --pdf "$out.pdf" --html "$out.html" --output "$out.txt"
+status=$?
+bzip2 < "$out.html" > "$out.html.bz2"
+echo "Done processing $ttf"
+exit $status
