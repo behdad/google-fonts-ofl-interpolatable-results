61684ed4f7a3bc2f560a680465c10401fb2034b7
Behdad Esfahbod <behdad@behdad.org>
2023-11-20 13:53:11 -0700

    Add 'git difftool --tool' tool

---
+++ diff-ttf-html-gz.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+dirname=`dirname "$0"`
+
+dir=`mktemp -d`
+
+echo "Uncompressing $1 and $2 to $dir"
+gunzip -c "$1" > "$dir/1.html"
+gunzip -c "$2" > "$dir/2.html"
+
+echo "Diffing $dir/1.html and $dir/2.html to $dir/diff.html"
+"$dirname/html-diff.sh" "$dir/1.html" "$dir/2.html" > "$dir/diff.html"
+
+#rm -f "$dir/1.html" "$dir/2.html"
+
+xdg-open "$dir/diff.html"
