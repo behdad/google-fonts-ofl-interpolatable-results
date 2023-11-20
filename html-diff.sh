e4e32b38028fd163d01a3d0225f0add93eedee86
Behdad Esfahbod <behdad@behdad.org>
2023-11-20 13:04:17 -0700

    [html-diff] Add header/footer

--- html-diff.sh
+++ html-diff.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-exec diff \
+echo "<!DOCTYPE html>"
+echo "<html><body align=center>"
+diff \
 	--unchanged-line-format='' \
 	--old-line-format='<span style="font-size: 100pt; color: red">-</span>%L' \
 	--new-line-format='<span style="font-size: 100pt; color: green">+</span>%L' \
 	"$@"
+echo "</body></html>"
