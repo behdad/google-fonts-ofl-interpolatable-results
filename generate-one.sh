55895b5229c32f77324b1b6821d0efc019b7e348
Behdad Esfahbod <behdad@behdad.org>
2023-11-20 16:55:44 -0700

    [generate-one] Minor

--- generate-one.sh
+++ generate-one.sh
@@ -5,8 +5,12 @@ ttf=$1
 basename=`basename $ttf`
 out="$dirname/reports/$basename"
 
-"$dirname"/submodules/fonttools/fonttools varLib.interpolatable "$ttf" --pdf "$out.pdf" --html "$out.html" --output "$out.txt"
-status=$?
+"$dirname"/submodules/fonttools/fonttools varLib.interpolatable \
+	"$ttf" \
+	--pdf "$out.pdf" \
+	--html "$out.html" \
+	--output "$out.txt" &&
 bzip2 < "$out.html" > "$out.html.bz2"
+status=$?
 echo "Done processing $ttf"
 exit $status
