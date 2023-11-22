cc6a056107b94224ca8d6587365f23d54a22a435
Behdad Esfahbod <behdad@behdad.org>
2023-11-22 07:28:09 -0700

    [diff] Put the family name in the diff file name
    And clean up title.

--- diff-ttf-html-bz2.sh
+++ diff-ttf-html-bz2.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 dirname=`dirname "$0"`
+basename=`basename "$3" .html.bz2`
 
 dir=`mktemp -d`
 
@@ -9,8 +10,8 @@ bunzip2 -c "$1" > "$dir/1.html"
 bunzip2 -c "$2" > "$dir/2.html"
 
 echo "Diffing $dir/1.html and $dir/2.html to $dir/diff.html"
-"$dirname/html-diff.sh" --title "$3" "$dir/1.html" "$dir/2.html" > "$dir/diff.html"
+"$dirname/html-diff.sh" --title "$basename diff" "$dir/1.html" "$dir/2.html" > "$dir/$basename-diff.html"
 
 rm -f "$dir/1.html" "$dir/2.html"
 
-xdg-open "$dir/diff.html"
+xdg-open "$dir/$basename-diff.html"
