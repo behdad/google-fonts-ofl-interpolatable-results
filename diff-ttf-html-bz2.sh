b79027ba19386b07dc0680d721db09dc3daac351
Behdad Esfahbod <behdad@behdad.org>
2023-11-24 13:11:11 -0700

    Show diffstat in the diff

--- diff-ttf-html-bz2.sh
+++ diff-ttf-html-bz2.sh
@@ -5,13 +5,16 @@ basename=`basename "$3" .html.bz2`
 
 dir=`mktemp -d`
 
+before="$dir/$basename.before.html"
+after="$dir/$basename.after.html"
+
 echo "Uncompressing $1 and $2 to $dir"
-bunzip2 -c "$1" > "$dir/1.html"
-bunzip2 -c "$2" > "$dir/2.html"
+bunzip2 -c "$1" > "$before"
+bunzip2 -c "$2" > "$after"
 
 echo "Diffing $dir/1.html and $dir/2.html to $dir/diff.html"
-"$dirname/html-diff.sh" --title "$basename diff" "$dir/1.html" "$dir/2.html" > "$dir/$basename-diff.html"
+"$dirname/html-diff.sh" --title "$basename diff" "$before" "$after" > "$dir/$basename-diff.html"
 
-rm -f "$dir/1.html" "$dir/2.html"
+rm -f "$before" "$after"
 
 xdg-open "$dir/$basename-diff.html"
--- html-diff.sh
+++ html-diff.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
-echo "<!DOCTYPE html>"
-echo "<html><body align=center>"
-
 if [ $# -lt 2 ]; then
 	echo "Usage: $0 <old-file>.html <new-file>.html"
 	exit 1
@@ -13,9 +10,20 @@ if [ "$1" = "--title" ]; then
 	shift 2
 fi
 
+echo "<!DOCTYPE html>"
+echo "<html><body align=center>"
+
+# Diffing twice; oh well. Maybe write the second as a sed...
+
+echo '<pre style="font-size: 150%">'
+diff "$@" | diffstat | sed 's/unknown *| *//; s/\(\+*\)\(-*\)$/<span style="color: #0B0">\1<\/span><span style="color: #B00">\2<\/span>/'
+echo '</pre>'
+
 diff \
 	--unchanged-line-format='' \
 	--old-line-format='<div style="background: #F88">%L</div>' \
 	--new-line-format='<div style="background: #8F8">%L</div>' \
 	"$@"
+
+cat "$tempfile"
 echo "</body></html>"
