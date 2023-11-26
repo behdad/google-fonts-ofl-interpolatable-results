cb4e5b9ec2b1e9bfdcce50862b3e928f97f4308f
Behdad Esfahbod <behdad@behdad.org>
2023-11-26 15:52:27 -0700

    [report-all] Add --dry-run

--- report-all.sh
+++ report-all.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
+#
+dryrun=
+while [ $# -gt 0 ]; do
+	case "$1" in
+	--dry-run)
+		dryrun=--dry-run
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
 
 dirname=$(dirname $0)
 # If there are any arguments, use them as directories to search
@@ -12,4 +25,4 @@ fi
 # GitHub doesn't like us making too many requests too quickly.
 # So, no parallelizing, and sleep in between.
 find $defaultdir "$@" -name '*.ttf.metadata' |
-xargs --verbose -I {} $dirname/report-one.sh --sleep "{}"
+xargs --verbose -I {} $dirname/report-one.sh --sleep $dryrun "{}"
