1b5afde7fe6c4e99e14b79fc320dc8e7f3b99bd4
Behdad Esfahbod <behdad@behdad.org>
2023-11-28 09:24:03 -0700

    [html-diff] Make work on Mac

--- html-diff.sh
+++ html-diff.sh
@@ -13,13 +13,25 @@ fi
 echo "<!DOCTYPE html>"
 echo "<html><body align=center>"
 
+diff=diff
+test=`$diff --unchanged-line-format='' /dev/null /dev/null`
+if [ x$? != x0 ]; then
+	if [ -f /opt/homebrew/bin/diff ]; then
+		diff=/opt/homebrew/bin/diff
+	else
+		echo "GNU diff not found; if on Mac, try 'brew install diffutils'"
+		exit 1
+	fi
+fi
+
+
 # Diffing twice; oh well. Maybe write the second as a sed...
 
 echo '<pre style="font-size: 150%">'
-diff "$@" | diffstat | sed 's/unknown *| *//; s/\(\+*\)\(-*\)$/<span style="color: #0B0">\1<\/span><span style="color: #B00">\2<\/span>/'
+$diff "$@" | diffstat | sed 's/unknown *| *//; s/\(\+*\)\(-*\)$/<span style="color: #0B0">\1<\/span><span style="color: #B00">\2<\/span>/'
 echo '</pre>'
 
-diff \
+$diff \
 	--unchanged-line-format='' \
 	--old-line-format='<div style="background: #F88">%L</div>' \
 	--new-line-format='<div style="background: #8F8">%L</div>' \
