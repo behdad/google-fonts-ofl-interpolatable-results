d235e77bbc7b001ee59b25349a042857d31a2fd6
Behdad Esfahbod <behdad@behdad.org>
2023-11-29 16:47:39 -0500

    Update google-fonts

--- review-all.sh
+++ review-all.sh
@@ -1,15 +1,9 @@
 #! /bin/bash
 
-dirname=$(dirname $0)
-# If there are any arguments, use them as directories to search
-# Otherwise, use the default directory
-defaultfiles=
-if [ $# -eq 0 ]; then
-	# No arguments supplied, using default directory
-	defaultfiles=$dirname/reports/*.html.bz2
-fi
+dirname=`dirname $0`
+cd "$dirname/reports"
 
-$(git diff HEAD $defaultfiles "$@" | diffstat | grep -q binary) &&
+git diff HEAD *.ttf.html.bz2 | diffstat | grep -q binary &&
 yes |
 git difftool --tool=ttf-html-bz2 HEAD \
-	$(git diff HEAD $defaultfiles "$@" | diffstat | grep binary | cut -d' ' -f2)
+	$(git diff HEAD *.ttf.html.bz2 | diffstat | grep binary | cut -d' ' -f2)
