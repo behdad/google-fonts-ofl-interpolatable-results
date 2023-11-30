0929798bd64dd2a573b2c99c8a2b4027e8598a4d
Behdad Esfahbod <behdad@behdad.org>
2023-11-30 16:24:39 -0500

    [review-one] Add --commit

--- review-one.sh
+++ review-one.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
+commit=HEAD
+while [ $# -gt 0 ]; do
+	case "$1" in
+	--commit)
+		commit=$2
+		shift
+		;;
+	*)
+		break
+		;;
+	esac
+	shift
+done
+
 yes |
-git difftool --tool=ttf-html-bz2 HEAD "$@"
+git difftool --tool=ttf-html-bz2 "$commit" "$@"
